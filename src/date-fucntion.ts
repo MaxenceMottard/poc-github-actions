4f2d270e8ebfa0e5e775a07d4a9bdcd1914a0264
MaxenceMottard <mottard.maxence@gmail.com>
2020-11-05 10:04:19 +0100

    Init the project

---
+++ src/date-fucntion.ts
@@ -0,0 +1,12 @@
+
+
+const days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
+const formatDate = (date: string): string => {
+  const dateObject = new Date(date);
+
+  return `${days[dateObject.getDay()]} ${dateObject.getDate()} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
+};
+
+export default formatDate;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import formatDate from "./date-fucntion";
+
+
+console.log(formatDate("1999-04-14"));
