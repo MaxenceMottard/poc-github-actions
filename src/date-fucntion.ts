4d5b78a05c185ad87b0136866da704c8bd3b99ee
MaxenceMottard <mottard.maxence@gmail.com>
2020-11-05 12:03:37 +0100

    test

--- src/date-fucntion.ts
+++ src/date-fucntion.ts
@@ -1,5 +1,5 @@
 const days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
-const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', "Juin", 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
 
 const formatDate = (date: string): string => {
   const dateObject = new Date(date);
